ffe4ed7d3392a3a5d83e82667b776e38562f0e59
Dennis Remme <dennis@remme.hamburg>
2018-04-12 17:53:46 +0200

    Downgrade to Java 1.8 for now.
    Waiting for Lombok and JITPACK to upgrade to 10.

--- src/main/java/hamburg/remme/tinyinjector/Injector.java
+++ src/main/java/hamburg/remme/tinyinjector/Injector.java
@@ -6,12 +6,10 @@ import static java.util.stream.Collectors.toList;
 import static java.util.stream.StreamSupport.stream;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Parameter;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,6 +18,12 @@ import java.util.*;
 import java.util.jar.JarFile;
 import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import lombok.val;
 
 /**
  * TinyInjector aims to be a very light weight dependency injection library. For that reason it is written in Java and
@@ -43,10 +47,10 @@ import java.util.zip.ZipEntry;
  * To automatically scan for these classes just call:
  *
  * <pre>
- * import static hamburg.remme.tinyinjector.Injector.scan;
+ * import  hamburg.remme.tinyinjector.Injector.scan;
  * import hamburg.remme.tinyinjector.Component;
  *
- * public static final main(String[] args) {
+ * public  final main(String[] args) {
  *     scan(Component.class, "my.package");
  * }
  * </pre>
@@ -56,25 +60,26 @@ import java.util.zip.ZipEntry;
  * To retrieve a singleton just call:
  *
  * <pre>
- * import static hamburg.remme.tinyinjector.Injector.scan;
+ * import  hamburg.remme.tinyinjector.Injector.scan;
  * import hamburg.remme.tinyinjector.Component;
  *
- * public static final main(String[] args) {
+ * public  final main(String[] args) {
  *     scan(Component.class, "my.package");
  *     var foo = retrieve(Foo.class);
  *     foo.beep();
  * }
  * </pre>
  *
+ *
+ * @todo: migrate to Java 10 asap; depends on JITPACK and Lombok
+ * @todo: move to MavenCentral? Bit cumbersome...
+ *
  * @author Dennis Remme (dennis@remme.hamburg)
  */
-public final class Injector {
+@UtilityClass public class Injector {
 
-    private static final String CLASS_EXTENSION = ".class";
-    private static Map<Class<?>, Object> DEPENDENCY_MAP;
-
-    private Injector() {
-    }
+    private final String CLASS_EXTENSION = ".class";
+    private Map<Class<?>, Object> DEPENDENCY_MAP;
 
     /**
      * Scans the class-path for classes annotated with {@link Component}, but only if they are inside the given
@@ -86,7 +91,7 @@ public final class Injector {
      * @throws IllegalStateException    when the class-path has already been scanned
      * @see #retrieve(Class)
      */
-    public static void scan(String packageName) {
+    public void scan(@NonNull String packageName) {
         scan(Component.class, packageName);
     }
 
@@ -99,14 +104,15 @@ public final class Injector {
      * @throws IllegalStateException    when the class-path has already been scanned
      * @see #retrieve(Class)
      */
-    public static void scan(Class<? extends Annotation> annotationClass, String packageName) {
+    public void scan(@NonNull Class<? extends Annotation> annotationClass, @NonNull String packageName) {
         if (DEPENDENCY_MAP != null) throw new IllegalStateException("Class-path has already been scanned.");
 
-        String basePath = packageName.replace('.', '/');
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        val basePath = packageName.replace('.', '/');
+        val classLoader = Thread.currentThread().getContextClassLoader();
 
         // Scan classes with attached annotation
-        Stream<String> classNames = isExecutingJar() ? scanJar(basePath) : scanFiles(basePath, classLoader);
+        val classNames = isExecutingJar() ? scanJar(basePath) : scanFiles(basePath, classLoader);
+        // val not working here; generics bug?
         Stream<? extends Class<?>> classes = classNames.map(it -> substringAfter(it, basePath).replace('/', '.'))
                 .map(it -> packageName + "." + it)
                 .map(it -> it.substring(0, it.length() - CLASS_EXTENSION.length()))
@@ -114,18 +120,18 @@ public final class Injector {
                 .filter(it -> it.isAnnotationPresent(annotationClass));
 
         // Create graph nodes
-        Set<ClassNode> graph = new HashSet<>();
+        val graph = new HashSet<ClassNode>();
         classes.forEach(clazz -> getOrCreate(graph, clazz).neighbors
                 .addAll(primaryParameters(clazz).stream()
                         .map(it -> getOrCreate(graph, it.getType()))
                         .collect(toList())));
-        Map<ClassNode, Integer> indegree = new HashMap<>();
+        val indegree = new HashMap<ClassNode, Integer>();
         graph.forEach(it -> indegree.put(it, 0));
         graph.stream().flatMap(it -> it.neighbors.stream()).forEach(it -> indegree.put(it, indegree.get(it) + 1));
 
         // Topologically sort the nodes
-        List<Class<?>> sorted = new ArrayList<>();
-        LinkedList<ClassNode> queue = new LinkedList<>();
+        val sorted = new ArrayList<Class<?>>();
+        val queue = new LinkedList<ClassNode>();
 
         graph.stream().filter(it -> indegree.get(it) == 0).forEach(it -> {
             queue.offer(it);
@@ -148,7 +154,7 @@ public final class Injector {
         DEPENDENCY_MAP = new HashMap<>();
         sorted.forEach(clazz -> {
             try {
-                Object[] arguments = primaryParameters(clazz).stream().map(it -> DEPENDENCY_MAP.get(it.getType())).toArray();
+                val arguments = primaryParameters(clazz).stream().map(it -> DEPENDENCY_MAP.get(it.getType())).toArray();
                 DEPENDENCY_MAP.put(clazz, primaryConstructor(clazz).newInstance(arguments));
             } catch (NullPointerException e) {
                 throw new IllegalArgumentException("Missing dependency.", e);
@@ -158,39 +164,35 @@ public final class Injector {
         });
     }
 
-    private static Stream<String> scanJar(String basePath) {
+    private Stream<String> scanJar(String basePath) {
         return asStream(getJar().entries())
                 .map(ZipEntry::getName)
                 .filter(it -> it.startsWith(basePath))
                 .filter(it -> it.toLowerCase().endsWith(CLASS_EXTENSION));
     }
 
-    private static Stream<String> scanFiles(String basePath, ClassLoader classLoader) {
-        try {
-            return asStream(classLoader.getResources(basePath))
-                    .map(URL::getFile)
-                    .map(Paths::get)
-                    .flatMap(Injector::walk)
-                    .map(Path::toString)
-                    .filter(it -> it.toLowerCase().endsWith(CLASS_EXTENSION));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    @SneakyThrows private Stream<String> scanFiles(String basePath, ClassLoader classLoader) {
+        return asStream(classLoader.getResources(basePath))
+                .map(URL::getFile)
+                .map(Paths::get)
+                .flatMap(Injector::walk)
+                .map(Path::toString)
+                .filter(it -> it.toLowerCase().endsWith(CLASS_EXTENSION));
     }
 
-    private static ClassNode getOrCreate(Set<ClassNode> graph, Class<?> clazz) {
+    private ClassNode getOrCreate(Set<ClassNode> graph, Class<?> clazz) {
         return graph.stream().filter(it -> it.value.equals(clazz)).findAny().orElseGet(() -> {
-            ClassNode node = new ClassNode(clazz);
+            val node = new ClassNode(clazz);
             graph.add(node);
             return node;
         });
     }
 
-    private static Constructor primaryConstructor(Class<?> clazz) {
+    private Constructor primaryConstructor(Class<?> clazz) {
         return clazz.getDeclaredConstructors()[0];
     }
 
-    private static List<Parameter> primaryParameters(Class<?> clazz) {
+    private List<Parameter> primaryParameters(Class<?> clazz) {
         return Arrays.asList(primaryConstructor(clazz).getParameters());
     }
 
@@ -200,59 +202,51 @@ public final class Injector {
      * @throws IllegalStateException    when no classes have been scanned yet
      * @throws IllegalArgumentException when the class has not been scanned
      */
-    @SuppressWarnings("unchecked") public static <T> T retrieve(Class<T> clazz) {
+    @SuppressWarnings("unchecked") public <T> T retrieve(@NonNull Class<T> clazz) {
         if (DEPENDENCY_MAP == null) throw new IllegalStateException("Retrieve called before scanning class-path.");
         if (!DEPENDENCY_MAP.containsKey(clazz)) throw new IllegalArgumentException("No such singleton.");
         return (T) DEPENDENCY_MAP.get(clazz);
     }
 
-    private static boolean isExecutingJar() {
+    private boolean isExecutingJar() {
         return Injector.class.getResource(Injector.class.getSimpleName() + ".class").toExternalForm().startsWith("jar");
     }
 
-    private static JarFile getJar() {
-        try {
-            return new JarFile(new File(Injector.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
-        } catch (IOException | URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
+    @SneakyThrows private JarFile getJar() {
+        return new JarFile(new File(Injector.class.getProtectionDomain().getCodeSource().getLocation().toURI()));
     }
 
-    private static Stream<Path> walk(Path path) {
-        try {
-            return Files.walk(path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    @SneakyThrows private Stream<Path> walk(Path path) {
+        return Files.walk(path);
     }
 
-    private static String substringAfter(String source, String delimiter) {
+    private String substringAfter(String source, String delimiter) {
         return source.substring(source.lastIndexOf(delimiter) + delimiter.length() + 1);
     }
 
-    private static <T> Stream<T> asStream(Enumeration<T> enumeration) {
-        return stream(spliteratorUnknownSize(enumeration.asIterator(), ORDERED), false);
+    private <T> Stream<T> asStream(Enumeration<T> enumeration) {
+        return stream(spliteratorUnknownSize(new Iterator<T>() {
+            @Override public boolean hasNext() {
+                return enumeration.hasMoreElements();
+            }
+
+            @Override public T next() {
+                return enumeration.nextElement();
+            }
+        }, ORDERED), false);
     }
 
-    private static Class<?> asClass(String className, ClassLoader classLoader) {
-        try {
-            return Class.forName(className, false, classLoader);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    @SneakyThrows private Class<?> asClass(String className, ClassLoader classLoader) {
+        return Class.forName(className, false, classLoader);
     }
 
     /**
      * Used for topological sort.
      */
-    static class ClassNode {
+    @RequiredArgsConstructor class ClassNode {
 
-        Class<?> value;
-        List<ClassNode> neighbors = new ArrayList<>();
-
-        ClassNode(Class<?> value) {
-            this.value = value;
-        }
+        final Class<?> value;
+        final List<ClassNode> neighbors = new ArrayList<>();
 
     }
 
